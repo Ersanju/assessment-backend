24b0cb98be92f98a3fc935e43b3b0cb0644aaae7
Sanjay Kumar <ersanjay426@gmail.com>
2024-12-29 04:12:57 +0530

    Initial project setup

---
+++ src/main/java/com/assessment/AssessmentBackendApplication.java
@@ -0,0 +1,14 @@
+package com.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentBackendApplication.class, args);
+		System.out.println("Application is runnning...");
+	}
+
+}
