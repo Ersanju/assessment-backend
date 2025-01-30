1f3680b81e6b28d469297060562247fd75823776
Sanjay Kumar <ersanjay426@gmail.com>
2025-01-30 17:30:34 +0530

    Add logging to the API requests

--- src/main/java/com/assessment/controller/UserController.java
+++ src/main/java/com/assessment/controller/UserController.java
@@ -2,6 +2,8 @@ package com.assessment.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,10 +22,12 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping("/load-users")
     @Operation(summary = "Load users data", description = "Load users data into in-memory H2 database")
     public String loadUsers() {
+        logger.info("Request to load users data into in-memory H2 database");
         userService.loadUsers();
         return "Users data loaded into in-memory H2 database successfully!";
     }
@@ -31,12 +35,14 @@ public class UserController {
     @GetMapping("/all-users")
     @Operation(summary = "Get all users", description = "Get all users from in-memory H2 database")
     public ResponseEntity<List<User>> getAllUsers() {
+        logger.info("Request to get all users from in-memory H2 database");
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
     @GetMapping("/role/{role}")
     @Operation(summary = "Get users by role", description = "Get users by role from in-memory H2 database")
     public ResponseEntity<List<User>> getUsersByRole(@PathVariable String role) {
+        logger.info("Request to get users by role from in-memory H2 database");
         return ResponseEntity.ok(userService.getUsersByRole(role));
   
     }
@@ -44,16 +50,20 @@ public class UserController {
     @GetMapping("/sorted")
     @Operation(summary = "Get users sorted by age", description = "Get users sorted by age from in-memory H2 database")
     public ResponseEntity<List<User>> getUsersSortedByAge(@RequestParam boolean ascending) {
+        logger.info("Request to get users sorted by age from in-memory H2 database");
         return ResponseEntity.ok(userService.getUsersSortedByAge(ascending));
     }
 
     @GetMapping("/user/{idOrSsn}")
     @Operation(summary = "Get user by ID or SSN", description = "Get user by ID or SSN from in-memory H2 database")
     public ResponseEntity<User> getUserByIdOrSsn(@PathVariable String idOrSsn) {
+        logger.info("Request to get user by ID or SSN from in-memory H2 database");
         try {
+            logger.info(idOrSsn + " is a number, trying to get user by ID");
             Long id = Long.parseLong(idOrSsn);
             return ResponseEntity.ok(userService.getUserByIdOrSsn(id, null));
         } catch (NumberFormatException e) {
+            logger.info(idOrSsn + "is not a number, trying to get user by SSN");
             return ResponseEntity.ok(userService.getUserByIdOrSsn(null, idOrSsn));
         }
     }
--- src/main/java/com/assessment/service/UserService.java
+++ src/main/java/com/assessment/service/UserService.java
@@ -2,6 +2,8 @@ package com.assessment.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -15,32 +17,44 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     public void loadUsers() {
-        
+        logger.info("Loading users data into in-memory H2 database");
         RestTemplate restTemplate = new RestTemplate();
         String url = "https://dummyjson.com/users";
-        UserResponse response = restTemplate.getForObject(url, UserResponse.class);
 
-        if (response != null) {
-            userRepository.saveAll(response.getUsers());
+        try {
+            UserResponse response = restTemplate.getForObject(url, UserResponse.class);
+            if (response != null) {
+                userRepository.saveAll(response.getUsers());
+                logger.info("Successfully saved {} users to database", response.getUsers().size());
+            } else {
+                logger.warn("No users found in the response");
+            }
+        } catch(Exception e) {
+            logger.error("Error occurred while loading users data: {}", e.getMessage(), e);   
         }
     }
 
     public List<User> getAllUsers() {
+        logger.debug("Fetching all users from in-memory H2 database");
         return userRepository.findAll();
     }
     
     public List<User> getUsersByRole(String role) {
+        logger.debug("Fetching users by role {}", role);
         return userRepository.findByRole(role);
     }
 
     public List<User> getUsersSortedByAge(boolean ascending) {
+        logger.debug("Fetching users sorted by age in {} order", ascending ? "ascending" : "descending");
         Sort sort = ascending ? Sort.by("age").ascending() : Sort.by("age").descending();
         return userRepository.findAll(sort);
     }
     
     public User getUserByIdOrSsn(Long id, String ssn) {
+        logger.debug("Fetching user by ID or SSN");
         return id != null ? userRepository.findById(id).orElse(null) 
                           : userRepository.findBySsn(ssn).orElse(null);
     }
