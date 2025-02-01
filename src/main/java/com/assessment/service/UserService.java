cefb53d37a4a6ae2e2eb35305b2c74f1478f1138
Sanjay Kumar <ersanjay426@gmail.com>
2025-02-02 03:00:24 +0530

    Some code fix

--- src/main/java/com/assessment/service/UserService.java
+++ src/main/java/com/assessment/service/UserService.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import com.assessment.dto.UserResponse;
